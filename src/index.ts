25f83aea45b6f7f7a938734fafe5a7cc8cfe8647
Andres <andreseg65@gmail.com>
2025-06-03 17:05:54 -0300

    Servidor funcional e iniciado con rutas conectadas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+import express from "express";
+import bookRoutes from "./routes/bookRoutes";
+import { connectDb } from "./config/db";
+
+process.loadEnvFile();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor HTTP en escucha en el puerto ${PORT}`);
+  connectDb();
+});
